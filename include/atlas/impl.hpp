3aabe52297c3b297098e5009b2b25057f8c1251b
lg <abbix@riseup.net>
2024-11-25 20:17:34 -0500

    initial commit

--- include/atlas/impl.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace Atlas::Impl {
-
-[[noreturn]] void panic(const char *message);
-
-}
--- include/atlas/io/error.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace Atlas::Io {
-enum Error { InvalidFile, OutOfBounds, EndOfFile };
-}
