c8f52eb5397bd239b46a5c428a04246ab3a009df
lg <abbix@riseup.net>
2024-11-28 19:35:31 -0500

    add example source file

---
+++ example/include/file.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+/// This is an example structure
+struct MyStruct {
+  int a;
+};
+
+/// This is another example structure used to demonstrate cross-references
+/// between types
+struct OtherStruct {
+  /// Something
+  MyStruct b;
+};
+
+/// A function that does nothing
+/// - `a`: First parameter
+/// - `b`: Second parameter
+void function(int a, OtherStruct b);
